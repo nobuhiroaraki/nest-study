b10bec9a3713fc433c7dc29fe60cd1ef484f4c5c
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-05-14 00:52:31 +0900

    CRUDの実装

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,30 @@
+import { ItemStatus } from './item-status.enum';
+import { Item } from './../../dist/items/item.mode.d';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'this is itemsService';
+  private items: Item[] = [];
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+
+  delete(id: string): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
