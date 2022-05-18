36a1edd23b0ce3a024b42304d4c2d46f2e6e4b17
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-05-19 01:42:24 +0900

    add repository

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
+import { ItemRepository } from './item.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ItemRepository])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
