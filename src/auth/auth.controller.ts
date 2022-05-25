0e340abf2b563bdafa8863893362eb2e0481ca06
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-05-26 00:36:19 +0900

    auth.controllerの追加

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { User } from './../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authService.signUp(createUserDto);
+  }
+}
