36fe03b4b445b21e676ea22fff3f5291c283011e
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-05-26 00:35:54 +0900

    user.repositoryの追加

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
+import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,14 @@
+import { User } from './../entities/user.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateUserDto } from '../auth/dto/create-user.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { username, password, status } = createUserDto;
+    const user = this.create({ username, password, status });
+
+    await this.save(user);
+    return user;
+  }
+}
