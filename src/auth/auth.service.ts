0ce5a328ee9ecf2b96f4868dc99642d5a2e80115
nobu_mac <nobuhiro.araki0513@gmail.com>
2022-05-26 00:36:07 +0900

    auth.serviceの追加

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
+import { User } from './../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UserRepository) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userRepository.createUser(createUserDto);
+  }
+}
